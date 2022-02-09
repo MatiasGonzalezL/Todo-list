bd276570d16fc036daa4ce037e25bb29294f6547
MatiasGonzalezL <matias.alejandro.gonzalezl@gmail.com>
2022-02-09 03:05:11 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { isEmpty } from "lodash";
+import React, { useState } from "react";
+import shortid from "shortid";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	const addTask = (e) => {
+		e.preventDefault();
+		if (isEmpty(task)) {
+			console.log("Task empty");
+			return;
+		}
+		const newTask = {
+			id: shortid.generate(),
+			name: task,
+		};
+
+		setTasks([...tasks, newTask]);
+
+		setTask("");
+	};
+
+	const deleteTask = (id) => {
+		const filteredTasks = tasks.filter((task) => task.id !== id);
+		setTasks(filteredTasks);
+	};
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>Todo List</h1>
+			<form className="encabezado" onSubmit={addTask}>
+				<div className="cuadroTexto">
+					<input
+						type="text"
+						className="inputText"
+						placeholder="Agrega una tarea aquí"
+						onChange={(text) => setTask(text.target.value)}
+						value={task}
+					/>
+				</div>
+			</form>
+			{tasks.length == 0 ? (
+				<h5> Aún no hay tareas añadidas</h5>
+			) : (
+				<ul className="linesHead">
+					{tasks.map((task) => (
+						<li className="lines" key={task.id}>
+							<span className="tarea">{task.name}</span>
+							<span
+								className="button"
+								onClick={() => deleteTask(task.id)}>
+								X
+							</span>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
